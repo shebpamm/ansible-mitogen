4dc7db739639728cd16d96a1427dc95ebb9c3549
Erik Karsten <erik.karsten@eficode.com>
2023-02-02 17:19:28 +0200

    add support for all default systems, instead of just x64-linux

--- flake.nix
+++ flake.nix
@@ -2,28 +2,31 @@
   description = "Nix flake that generates a configuration derivation and provides mitogen";
   inputs = {
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs-stable }:
-  {
+  outputs = { self, nixpkgs-stable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs-stable { inherit system; };
+      in
+      {
+        defaultPackage = pkgs.stdenv.mkDerivation
+          {
+            name = "ansible-mitogen";
+            srcs = [
+              (builtins.fetchTarball {
+                url = "https://github.com/mitogen-hq/mitogen/archive/refs/tags/v0.2.10.tar.gz";
+                sha256 = "110ppgvmnz7ykdx47x33c0rb7zq3fkrw6g699c4vqa28mn00qp28";
+                name = "mitogen";
+              })
+              self
+            ];
 
-  defaultPackage.x86_64-linux =
-    let
-      pkgs = import nixpkgs-stable { system = "x86_64-linux"; };
-    in pkgs.stdenv.mkDerivation {
-      name = "ansible-mitogen";
-      srcs = [
-        (builtins.fetchTarball {
-          url = "https://github.com/mitogen-hq/mitogen/archive/refs/tags/v0.2.10.tar.gz";
-          sha256 = "110ppgvmnz7ykdx47x33c0rb7zq3fkrw6g699c4vqa28mn00qp28";
-          name = "mitogen";
-        })
-        self
-      ];
+            sourceRoot = "mitogen";
 
-      sourceRoot = "mitogen";
-
-      installPhase = "mkdir -p $out; cp -r ./ansible_mitogen $out; sed s#install#$out#g ../source/ansible.tmpl > $out/ansible.cfg";
-    };
-  };
+            installPhase = "mkdir -p $out; cp -r ./ansible_mitogen $out; sed s#install#$out#g ../source/ansible.tmpl > $out/ansible.cfg";
+          };
+      }
+    );
 }
